5f3d34e18da27ba6c4b51bfdf1a787f1af0bb9cb
travis dewolf <travis.dewolf@gmail.com>
2018-09-11 15:39:24 -0400

    changes for different tests

--- abr_control/controllers/osc.py
+++ abr_control/controllers/osc.py
@@ -153,7 +153,7 @@ class OSC(controller.Controller):
             # using the rcond to set singular values < thresh to 0
             # is slightly faster than doing it manually with svd
             # singular values < (rcond * max(singular_values)) set to 0
-            Mx = np.linalg.pinv(Mx_inv, rcond=.005)
+            Mx = np.linalg.pinv(Mx_inv, rcond=.04)
         self.Mx = Mx
 
         u_task = np.zeros(3)  # task space control signal
